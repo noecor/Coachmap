79b2bbfa92ffdcc487e4b9da79aea91f2894b259
noecor <47372835+noecor@users.noreply.github.com>
2020-10-15 02:27:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+// JavaScript
+const numberOfPosts = 3;
+const getRes = () => {
+  fetch(
+    `https://blog.coachmap.me/wp-json/wp/v2/posts?_embed&per_page=` +
+      numberOfPosts
+  )
+    .then((res) => res.json())
+    .then((res) => getBlogs(res));
+};
+
+const getBlogs = (e) => {
+  let temparray = [];
+
+  e.forEach((elem) => {
+    let temp = [];
+    temp.title = elem.title.rendered;
+    temp.imageScr = elem._embedded["wp:featuredmedia"]["0"].source_url;
+    temp.link = elem.link;
+    temp.excerpt = elem.excerpt.rendered;
+    temparray.push(temp);
+  });
+
+  showResults(temparray);
+};
+
+const showResults = (list) => {
+  if (list.length <= 0) {
+    console.log("sin datos");
+    return null;
+  }
+
+  let loading = document.getElementById("loading");
+  loading.parentNode.removeChild(loading);
+
+  let container = document.getElementById("listRecent");
+
+  list.forEach((elem) => {
+    let bloggy = document.createElement("li");
+    bloggy.setAttribute("class", "listRecentItem");
+    bloggy.innerHTML = `<a href="${elem.link}>
+        <div class="listRecentItemImage">
+            <a href="${elem.link}"><img src="${elem.imageScr}" alt="${elem.title}" class="listRecentItemImage"></a>
+        </div>
+        <div class="listRecentItemTitle width300px">
+            <a href="${elem.link}><h4 class="p18px">${elem.title}</h4></a>
+        </div>
+        <div class="listRecentItemExcerpt p14px width300px">
+            <p>${elem.excerpt}</p>
+        </div>
+    </a>`;
+    container.appendChild(bloggy);
+  });
+};
+
+getRes();
+
+//href="${elem.link}" href="${elem.link}"
+//class="flexContainer"
